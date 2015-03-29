1df23a69d1d2f2e8ec12a95b29c555302626cee5
bolobob <aokishinichi@gmail.com>
2015-03-29 22:21:18 +0900

    都道府県ごとにオブジェクトを得た

--- japan.js
+++ japan.js
@@ -6,3 +6,7 @@ var svg = d3.select("body")
           .append("svg")
           .attr("width", w)
           .attr("height", h);
+d3.json("japan.topojson", function(json) {
+  var japan = topojson.object(json, json.objects.japan).geometries;
+  console.log(japan);
+});
