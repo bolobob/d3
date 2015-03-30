2e61fcfeae7cae9b220f41a48481f127e1ed9d03
bolobob <aokishinichi@gmail.com>
2015-03-31 01:05:47 +0900

    色を付けた

--- japan.js
+++ japan.js
@@ -1,5 +1,6 @@
 var w = 1000;
 var h = 1000;
+var color = d3.scale.category10();
 
 // SVG要素生成
 var svg = d3.select("body")
@@ -8,5 +9,40 @@ var svg = d3.select("body")
           .attr("height", h);
 d3.json("japan.topojson", function(json) {
   var japan = topojson.object(json, json.objects.japan).geometries;
-  console.log(japan);
+  // 投影法設定
+  var projection = d3.geo.mercator()
+                   .center([137, 34])
+                   .translate([w/2, h/2])
+                   .scale(1500);
+
+  // 緯度経度⇒パスデータ変換設定
+  var path = d3.geo.path()
+             .projection(projection);
+
+  // パスデータとして日本地図描画
+  svg.selectAll("path")
+  .data(japan)
+  .enter()
+  .append("path")
+  .attr("d", path)
+  .attr("stroke", "black")
+  .attr("stroke-width", 0.5)
+  .style("fill", function(d, i) {
+      return color(i);
+    });
+
+  // 都道府県名
+  svg.selectAll(".place-label")
+  .data(japan)
+  .enter()
+  .append("text")
+  .attr("font-size", "8px")
+  .attr("class", "place-label")
+  .attr("transform", function(d) {
+      var lat = d.properties.latitude;
+      var lng = d.properties.longitude;
+      return "translate(" + projection([lng, lat]) + ")";
+    })
+  .attr("dx", "-1.5em")
+  .text(function(d) { return d.properties.name_local; });
 });
